b189f5df67f8cfb4795e59169d0d7c2740384c1f
Mr Anthony <44789325+AnthonyDisasi@users.noreply.github.com>
2024-12-20 06:12:14 +0200

    DbContext and configuration of it

---
+++ DemoCrudDotNet9/Data/ProductDbContext.cs
@@ -0,0 +1,11 @@
+﻿using DemoCrudDotNet9.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DemoCrudDotNet9.Data
+{
+    public class ProductDbContext(DbContextOptions<ProductDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
--- DemoCrudDotNet9/Program.cs
+++ DemoCrudDotNet9/Program.cs
@@ -1,9 +1,15 @@
+using DemoCrudDotNet9.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ProductDbContext>(options => 
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DbConn"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
